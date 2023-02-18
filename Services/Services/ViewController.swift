00bcc54397340b132357ccd498e29ff645d57003
Dmitry Maslennikov <mrmda28@gmail.com>
2023-02-18 14:54:52 +0300

    Add a new project

---
+++ Services/Services/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Services
+//
+//  Created by Dmitry Maslennikov on 18.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
